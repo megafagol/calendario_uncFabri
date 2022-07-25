f94d303ac8e684a9dbcf5c00fd7b2e3dfb402f7b
Matias Ruiz <matiasignacioruiz@gmail.com>
2022-07-25 19:28:13 -0300

    listo nav

--- src/App.vue
+++ src/App.vue
@@ -2,22 +2,23 @@
   <div class="vue-tempalte">
     <!-- Navigation -->
 
-    <nav
-      class="navbar shadow bg-white rounded justify-content-between flex-nowrap flex-row fixed-top"
-    >
-      <h6 class="title2 col-12 mb-5 mx-2">UNC <span class="title-2">Calendar</span></h6>
+    <nav class="navbar">
+
+      <h4 class="title2 col-12 mb-5 mx-2">UNC<span class="title-2"> Calendario</span></h4>
 
 
-      <ul class="nav navbar-nav flex-row float-right">
+      <div class="nav">
 
-        <li class="nav-item">
-          <router-link class="rutaHome" to="/">Home</router-link>
-        </li>
+        <router-link  to="/">
+          <button class="rutaHome">Home</button>
+        </router-link>
+        
 
-        <li class="nav-item">
-          <router-link class="rutaHorario" to="/horarios">Horarios</router-link>
-        </li>
-      </ul>
+        <router-link  to="/horarios">
+          <button class="rutaHorario">Horarios</button>
+        </router-link>
+        
+      </div>
     </nav>
     <!-- Main -->
     <div class="App">
@@ -35,49 +36,58 @@
 </template>
 
 <style>
-nav {
+.navbar {
   margin: 0;
   padding: 0;
-  position: fixed;
+  position: relative;
   width: 100%;
-  height: 7%;
+  height: 3em;
+  background-color: #fff;
+  outline: 1px solid #000;
 }
 
-h6{
+h4{
   position: relative;
-  top: 80%;
+  top: 10%;
   left: 5%;
-  font-size:3em;
+  color:#2C5F66 !important;
 }
 span{
     color: #DDC77A;
 }
 
-ul {
-  list-style: none;
-  position: relative;
-  right: 10%;
-  width: 100%;
+.nav {
+  bottom: 4.5em;
+  width: 20em;
+  display: flex;
+  flex-direction: row;
+  justify-content:space-between;
 }
-li {
-  list-style: none;
-  height:10%;
-  width:100%;
-  display: float;
-}
+
 .rutaHome{
-  float: left;
+  position: relative;
+  left:62em;
+  bottom: 4.5em;
   text-decoration: none;
   color: #2C5F66;
   width: 30%;
+  background-color:#DDC77A;
+  border:none;
+  border-radius: 8%;
+  width: 6em;
 }
 
 .rutaHorario{
-  display: relative;
-  right:30%;
-  float: right;
+  position: relative;
+  left:55em;
+  bottom: 4.5em;
   text-decoration: none;
   color: #2C5F66;
   width: 30%;
+  background-color:#DDC77A;
+  border:none;
+  z-index:100;
+  border-radius: 8%;
+  width: 6em;
 }
 </style>
