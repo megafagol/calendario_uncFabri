c869cba184b3061b57351eaeecc9a79207a5c697
megafagol <fabriziorecchini9908@gmail.com>
2023-08-15 13:26:16 -0300

    update url endpoint back

--- src/utils/axios.js
+++ src/utils/axios.js
@@ -3,7 +3,7 @@ import axios from 'axios';
 axios.defaults.headers.common['Authorization'] = `Basic YWRtaW46NzUzOTUx`;
 
 export const http = axios.create({
-    baseURL: 'http://50.16.25.112:8080/',
+    baseURL: 'https://backffabri-ksdb-dev.fl0.io/',
     timeout: 2000000
 });
 
